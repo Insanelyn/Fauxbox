efb6b518755bda4566c1865cda81f26178a564dd
Christoffer Nygren <christoffernygren90@hotmail.com>
2018-05-30 12:39:50 +0200

    Added necessary comps and services

--- Fauxbox/src/app/app.module.ts
+++ Fauxbox/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { RowComponent } from './row/row.component';
+import { UploadComponent } from './upload/upload.component';
+import { DetailsComponent } from './details/details.component';
+import { NavComponent } from './nav/nav.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    RowComponent,
+    UploadComponent,
+    DetailsComponent,
+    NavComponent,
+    SidebarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
